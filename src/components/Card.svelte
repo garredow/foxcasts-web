9716472d280c7f1aadab0d07f1e08268fbce1546
Garrett Downs <garredow@outlook.com>
2022-08-06 16:06:15 -0400

    create card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<section>
+  <div class="title">{title}</div>
+  <slot />
+</section>
+
+<style>
+  section {
+    background-color: #fff;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    margin: 20px;
+    padding: 10px 15px 10px 15px;
+  }
+
+  section .title {
+    font-weight: 500;
+    text-transform: uppercase;
+    color: rgba(0, 0, 0, 0.5);
+    margin-bottom: 10px;
+    text-align: center;
+  }
+
+  section .actions {
+    display: flex;
+    justify-content: center;
+  }
+</style>
