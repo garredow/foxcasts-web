6778ffd7203e4f7b29d403224256e1a6f143f8be
Garrett Downs <garredow@outlook.com>
2022-08-06 17:54:21 -0400

    add icon components

---
+++ src/components/Icon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let size: number = 24;
+  export let disabled = false;
+</script>
+
+<div class:disabled style={`height: ${size}px; width: ${size}px;`}>
+  <slot />
+</div>
+
+<style>
+  .disabled {
+    opacity: 0.2;
+  }
+</style>
---
+++ src/components/IconButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte';
+  import Icon from './Icon.svelte';
+
+  export let icon: typeof SvelteComponent;
+  export let disabled = false;
+  export let onClick: () => void;
+</script>
+
+<button class="root" {disabled} on:click={onClick}>
+  <Icon size={24}><svelte:component this={icon} /></Icon>
+</button>
+
+<style>
+  .root {
+    outline: none;
+    border: none;
+    background: none;
+    width: 100%;
+    padding: 5px;
+    text-transform: uppercase;
+    font-weight: var(--bold-font-weight);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
