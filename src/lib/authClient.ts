10bf88410e3ee5d3babb30ff71e2fe31224e668a
Garrett Downs <garredow@outlook.com>
2022-08-07 07:20:46 -0400

    update auth0 settings

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -1,9 +1,9 @@
 import createAuth0Client from '@auth0/auth0-spa-js';
 
 export const authClient = await createAuth0Client({
-  domain: 'podcloud-dev.us.auth0.com',
-  client_id: 'SlTeN48AGrnMQL2F2iQN34Qi3zRuacQs',
+  domain: 'foxcasts.us.auth0.com',
+  client_id: '55YoJp810kRcRcavhU5RkzUuxRJs4gW9',
   redirect_uri: window.location.origin,
   scope: 'read:current_user update:current_user_metadata offline_access openid',
-  audience: 'https://api.podcloud.social',
+  audience: 'https://api.foxcasts.com',
 });
